83cabb0e903395f51a6730d1a2a5455d66a5409d
oenrique <oenrique.reyes@hotmail.com>
2025-08-20 10:06:45 -0600

    Con las opciones de menú

--- templates/header.php
+++ templates/header.php
@@ -25,10 +25,10 @@ require_once __DIR__ . '/../app/includes/config.php'; ?>
         </button>
         <div class="collapse navbar-collapse" id="mainNav">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link" href="#">Usuarios</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Proveedores</a></li>
+                <li class="nav-item"><a class="nav-link" href="gestion_usuarios.php">Usuarios</a></li>
+                <li class="nav-item"><a class="nav-link" href="gestion_proveedores.php">Proveedores</a></li>
                 <li class="nav-item"><a class="nav-link" href="#">Catálogos</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Contratos</a></li>
+                <li class="nav-item"><a class="nav-link" href="gestion_contratos.php">Contratos</a></li>
             </ul>
             <ul class="navbar-nav">
                 <li class="nav-item dropdown">
